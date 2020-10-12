cd6f37c864c841fa4c0e817b43f2fb261d863893
Joshua Price <jprice@berkeley.edu>
2020-10-11 23:59:59 -0700

    created generate_sim_data.py

---
+++ sim/generate_sim_data.py
@@ -0,0 +1,8 @@
+import settings
+
+class SimData:
+
+    def __init__(self, directory, num_haplotypes=5, num_markers=3):
+        self.directory = directory
+        self.num_haplotypes = num_haplotypes
+        self.num_markers = num_markers

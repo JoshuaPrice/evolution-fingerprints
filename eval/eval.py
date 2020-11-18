d0efc409b5bb7f690d4b9b694216c44ff32ac39f
Hyun Soo Jeon <hsjeon@stanford.edu>
2020-11-18 09:06:54 +0000

    added silhouette scores

--- eval/eval.py
+++ eval/eval.py
@@ -1,5 +1,5 @@
 '''
-Usage: python3 eval.py 1.csv 2.csv etc.
+Usage: python3 eval.py 1.csv 2.csv etc. (*.csv also works)
 The program will compute the evaluation metric for each clustering csv file.
 Format of input file: List of samples in each cluster separated by comma, one cluser per line
 Adjust the following POP_PATH_INIT and DATA_PATH_INIT for population data and raw SNP data if necessary.
@@ -9,12 +9,14 @@ Usage as module:
     ev.read_clusters(cluster_path)
 
     ev.calinski-harabasz()
+    ev.sum_of_squares()
+    ev.silhouette()
 
     (if using get_pop_dist())
     ev.init_pop(pop_path)
     ev.get_pop_dist()
 '''
-POP_PATH_INIT = None #if using ev.get_pop_dist(), set this as 'PATH_to_evfi/igsr_samples.tsv'
+POP_PATH_INIT = None #if using ev.get_pop_dist(), set this as 'PATH_TO_EVFI/igsr_samples.tsv'
 DATA_PATH_INIT = '/mnt/datadrive/data/full-data-matrix.csv'
 
 import sys
@@ -22,8 +24,6 @@ import sys
 import csv
 import numpy as np
 import pandas as pd
-
-import pdb
 
 class Eval:
 
@@ -80,6 +80,9 @@ class Eval:
         (variance between clusters) over (variance within clusters)
         >> [tr(between-matrix) / tr(within-matrix)] * [(num_samples - k) / (k - 1)]
         Bigger C-H score indicates more separation between clusters
+        Note: within_variance == sum_of_squares()
+
+        Returns: (within_variance, calinski_harabasz_score)
         '''
         within_clusters = np.zeros(shape=(self.num_features, self.num_features))
         between_clusters = np.zeros(shape=(self.num_features, self.num_features))
@@ -88,6 +91,7 @@ class Eval:
             cluster_elems = self.clusters[cluster_idx]
             cluster_data = self.data.loc[cluster_elems].to_numpy()
             cluster_center = np.average(cluster_data, axis=0)
+
             for elem_idx in range(len(cluster_elems)):
                 diff = cluster_data[elem_idx, :] - cluster_center
                 within_clusters += np.outer(diff, diff)
@@ -100,10 +104,91 @@ class Eval:
 
         return np.trace(within_clusters), score
 
-    def get_pop_dist(self, use_superpop=False):
+    def sum_of_squares(self):
+        '''
+        Returns the simple metric of sum of squares:
+        sum of squared distance from each datapoint to the corresponding cluster centroid
+        '''
+        sum_of_squares = 0
+
+        for cluster_idx in range(self.num_clusters):
+            cluster_elems = self.clusters[cluster_idx]
+            cluster_data = self.data.loc[cluster_elems].to_numpy()
+            cluster_center = np.average(cluster_data, axis=0)
+            for elem_idx in range(len(cluster_elems)):
+                diff = cluster_data[elem_idx, :] - cluster_center
+                sum_of_squares += np.sum(np.inner(diff, diff))
+
+        return sum_of_squares
+    
+    def silhouette(self):
+        '''
+        The silhouette score for each datapoint calculates:
+        (avg dist to points in the closest cluster) - (avg dist to points in the same cluster)
+        divided by the bigger of the two values.
+        The score is in range [-1, 1], where 1 denotes that the point is clustered well,
+        and -1 denotes that the point is closer to another cluster, with 0 being neutral.
+        
+        Returns: The average silhouette score of all datapoints
+        '''
+        within = []
+        between = []
+
+        pre_dict = {} # dictionary of sample to cluster(arbitrarily numbered)
+        for cluster_idx in range(self.num_clusters):
+            cluster_elems = self.clusters[cluster_idx]
+            for elem in cluster_elems:
+                pre_dict[elem] = cluster_idx
+
+        for cluster_idx in range(self.num_clusters):
+            cluster_elems = self.clusters[cluster_idx]
+            if len(cluster_elems) == 0:
+                continue
+            elif len(cluster_elems) == 1:
+                # silhouette = 0 if cluster size = 1
+                within.append(1)
+                between.append(1)
+                continue
+
+            for elem in cluster_elems:
+                dist_list = []
+                elem_data = self.data.loc[[elem]].to_numpy()
+
+                for compare_cluster_idx in range(self.num_clusters):
+                    compare_within = (compare_cluster_idx == cluster_idx)
+
+                    compare_cluster_elems = self.clusters[compare_cluster_idx]
+                    if len(compare_cluster_elems) == 0:
+                        continue
+                    
+                    compare_data = self.data.loc[compare_cluster_elems].to_numpy()
+                    diffs = compare_data - elem_data
+                    # einsum gives dot product by row
+                    dists = np.sqrt(np.einsum('ij, ij -> i', diffs, diffs))
+                    sum_dist = np.sum(dists)
+
+                    if compare_within:
+                        within.append(sum_dist / (len(cluster_elems) - 1))
+                    else:
+                        dist_list.append(sum_dist / len(compare_cluster_elems))
+
+                # choose the total distance to the closest cluster
+                between.append(min(dist_list))
+
+        silhouette_list = []
+        for ai, bi in zip(within, between):
+            silhouette_list.append((bi - ai) / max(ai, bi))
+        mean_sil = np.average(np.array(silhouette_list))
+
+        return mean_sil
+
+
+    def get_pop_dist(self, use_superpop=True):
         '''
         Returns a numpy array (num_clusters x num_populations)
         of percentages of each population in each cluster
+        use_superpop : denotes to use superpopulation(broader e.g. EURopean)
+                       instead of population(e.g. FINnish)
         '''
         pop_idx_dict = {}
         for pop, superpop in self.pops.values():
@@ -124,22 +209,31 @@ class Eval:
         return pop_dist
 
 
+def run_experiment(ev, cluster_path):
+    print("Path :", cluster_path)
+    
+    print("Silhouette: ", ev.silhouette())
+
+    within_var, ch_score = ev.calinski_harabasz()
+    # skip if score is nan
+    if np.isnan(ch_score):
+        print("has an undefined cluster; skipping...\n")
+        return
+    print("Calinski-Harabasz score", format(ch_score, ".2f"), \
+            "// within_variance", format(within_var, ".2f"))
+
+    # pop_dist = ev.get_pop_dist(use_superpop=True)
+    # print(pop_dist)
+
+
 def main(pop_path, data_path, cluster_paths):
     ev = Eval(data_path)
+    # ev.init_pop(pop_path) :: uncomment and set valid POP_PATH_INIT at top of file  if using ev.get_pop_dist()
 
     for cluster_path in cluster_paths:
         ev.read_clusters(cluster_path)
-        within_var, ch_score = ev.calinski_harabasz()
-        # skip if score is nan (i.e. there is an empty cluster)
-        if np.isnan(ch_score):
-            print("Path :", cluster_path, "\nhas an empty cluster; skipping...\n")
-            continue
-        print("Path :", cluster_path, "\nCalinski-Harabasz score", format(ch_score,".2f"), \
-                "// within_variance", format(within_var, ".2f"), "\n")
+        run_experiment(ev, cluster_path)
 
-        # ev.init_pop(pop_path)
-        # pop_dist = ev.get_pop_dist(use_superpop=True)
-        # print(pop_dist)
 
 if __name__ == "__main__":
     pop_path = POP_PATH_INIT
